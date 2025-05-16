89e58b8f0819604e898187b69156611982f5e070
Malik Laet <isklikma@gmail.com>
2025-05-15 23:07:14 -0300

    add route user

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,25 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import Message, UserDB, UserPublicSchema, UserSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get('/')
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Ola asdasd'}
+
+
+@app.post(
+    '/user/', status_code=HTTPStatus.CREATED, response_model=UserPublicSchema
+)
+def create_user(user: UserSchema):
+    user_whith_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_whith_id)
+
+    return user_whith_id
