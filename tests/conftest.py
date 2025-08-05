0e5a8d35f9f32a34e0a96b5b29cd8861bfd1c587
Malik Laet <isklikma@gmail.com>
2025-08-05 16:24:21 -0300

    refactoring all

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,6 +11,7 @@ from fast_zero.app import app
 from fast_zero.database import get_session
 from fast_zero.models import User, table_registry
 from fast_zero.security import get_password_hash
+from fast_zero.settings import Settings
 
 
 @pytest.fixture
@@ -23,6 +24,21 @@ def client(session):
         yield client
 
     app.dependency_overrides.clear()
+
+
+@pytest.fixture
+def user(session):
+    user = User(
+        username='Teste',
+        email='teste@test.com',
+        password=get_password_hash('testtest'),
+    )
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    user.clean_password = 'testtest'
+    return user
 
 
 @pytest.fixture
@@ -41,16 +57,20 @@ def session():
 
 
 @contextmanager
-def _mock_db_time(model, time=datetime(2025, 5, 27)):
+def _mock_db_time(*, model, time=datetime(2024, 1, 2)):
     def fake_time_hook(mapper, connection, target):
         if hasattr(target, 'created_at'):
             target.created_at = time
 
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
+
     event.listen(model, 'before_insert', fake_time_hook)
 
-    yield time
-
-    event.remove(model, 'before_insert', fake_time_hook)
+    try:
+        yield time
+    finally:
+        event.remove(model, 'before_insert', fake_time_hook)
 
 
 @pytest.fixture
@@ -59,25 +79,14 @@ def mock_db_time():
 
 
 @pytest.fixture
-def user(session):
-    password = 'testtest'
-    user = User(
-        username='Teste',
-        email='teste@test.com',
-        password=get_password_hash('testtest'),
-    )
-    session.add(user)
-    session.commit()
-    session.refresh(user)
-
-    user.clean_password = password
-    return user
-
-
-@pytest.fixture
 def token(client, user):
     response = client.post(
-        '/token',
+        'auth/token',
         data={'username': user.email, 'password': user.clean_password},
     )
     return response.json()['access_token']
+
+
+@pytest.fixture
+def settings():
+    return Settings()
